335fda147105336e39c21d02959dcd7acdc9fbca
zhangxu <zhangxu@daodao.ai>
2022-09-22 17:27:10 +0800

    修改配置

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ const cdnBasePath = 'https://storage-node.ipin.com/mece-new-h5/';
 export default defineConfig({
   jsMinifier: 'terser',
   cssMinifier: 'cssnano',
-  base: '/daodao',
+  base: '/',
   npmClient: 'yarn',
   alias: {
     '@': '../src',
@@ -26,17 +26,6 @@ export default defineConfig({
     chrome: 50,
   },
   define: {
-    runtimeConfigMap: {
-      eventApi:
-        process.env.NODE_ENV !== 'production'
-          ? 'https://pre-r.wmzy.com'
-          : 'https://r.wmzy.com',
-      staticPrefix: '/daodao',
-      channel_key: 'ios',
-      RequestTimeout: 3000,
-      PrefixApi: '/gw',
-      DevDomain: 'pre-m.daodao.pro',
-      Env: 'dev',
-    },
+    runtimeConfigMap: {},
   },
 });
